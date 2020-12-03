8281fcb7c6622cdfceeb5aed09ab9c4482a76758
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-12-03 21:41:33 +0900

    コントローラーアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+  
 end
