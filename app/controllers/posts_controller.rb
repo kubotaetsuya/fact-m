b552cbace9edc8e59b928195e059e7e761fbbfa3
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-12-04 00:03:06 +0900

    アクションの定義とストロングパラメーターの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,19 @@
 class PostsController < ApplicationController
 
   def index
-
+    @post = Post.all
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    if @post = Post.new(post_params)
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+          
+  validates :email, uniqueness: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :password, confirmation: true
+  with_options presence: true do
+    validates :password_confirmation
+    validates :nickname
+    validates :birthday
+  end
+  
+  has_many :posts
+  has_many :comments
 end
